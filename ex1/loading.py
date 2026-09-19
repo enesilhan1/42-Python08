420e0a839d4cba4c23de3ad2b8d8578d91efa0bd
Enes Ilhan <enilhan@k1m10s01.42istanbul.com.tr>
2026-09-19 03:04:59 +0300

    build: check_depencies function has been created and completed

--- ex1/loading.py
+++ ex1/loading.py
@@ -13,3 +13,15 @@ def get_version(name: str) -> str | None:
     except PackageNotFoundError:
         return
 
+def check_dependencies() -> list[str]:
+    missings: list[str] = []
+    for name, description in DEPENDENCIES.items():
+        vs = get_version(name)
+        if vs:
+            print(f"[OK] {name} ({vs}) - {description}")
+        else:
+            print(f"[KO] {name} missing")
+            missings.append(name)
+    return missings
+
+
